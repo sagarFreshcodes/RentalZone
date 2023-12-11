10d57504701dfb6e01637edd22780c11eabbf268
sagarFreshcodes <136316480+sagarFreshcodes@users.noreply.github.com>
2023-12-11 12:19:19 +0530

    UPDATE: code

--- src/Components/Pages/PrivateComponent/ProductDetails/ProductDetail.jsx
+++ src/Components/Pages/PrivateComponent/ProductDetails/ProductDetail.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
-
+import slide1 from "../../../../assets/images/Essential/ProductDetail/slide1.png";
+import slide2 from "../../../../assets/images/Essential/ProductDetail/slide2.png";
+import slide3 from "../../../../assets/images/Essential/ProductDetail/slide3.png";
+import slide4 from "../../../../assets/images/Essential/ProductDetail/slide4.png";
+import slide5 from "../../../../assets/images/Essential/ProductDetail/slide5.png";
 const ProductDetail = () => {
   return (
     <div className='pd_ProductDetail'>ProductDetail</div>
